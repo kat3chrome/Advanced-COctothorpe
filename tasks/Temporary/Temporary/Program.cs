7efbc5fec494c2c10e1bdb6cd44e011901db8fe1
ashumakov <>
2021-01-11 23:29:18 +0300

    Create solution and test

---
+++ tasks/Temporary/Temporary/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Temporary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
