92f52270b7338b2cba4dffda1efd0c434f3dde89
ashumakov <>
2021-01-16 23:37:14 +0300

    create basic stuff

---
+++ tasks/MyThreadPool/MyThreadPool/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyThreadPool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
