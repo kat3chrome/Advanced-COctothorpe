29f9221e11a21204c95fa1b9b2211058b99eb22a
ashumakov <>
2021-02-06 22:50:53 +0300

    Create IMyTask.cs

---
+++ tasks/MyThreadPool/MyThreadPool/IMyTask.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MyThreadPool
+{
+    public interface IMyTask<TResult>
+    {
+        public bool IsCompleted { get; }
+        
+        public TResult Result { get; }
+
+        public IMyTask<TNewResult> ContinueWith<TNewResult>(Func<TResult, TNewResult> func);
+    }
+}
